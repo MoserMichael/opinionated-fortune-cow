bdf78d7a3c2173814f4457f4f8929b5a6a889168
MichaelMoser <mmoser@dhcp-3-224.tlv.redhat.com>
2020-01-27 16:04:32 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+
+## opinionated fortune cow.
+
+pipes fortune into cowsay. Now the fortune cookie is passed through sentiment analysis, if the result is positive then the cow has a positive expression, if negative then the cow has a sad expression.
+
+The first run of this script creates a python virtual environment that installs nltk, that is needed for running the sentiment analysis step.
---
+++ cows.sh
@@ -0,0 +1,108 @@
+#!/bin/bash
+
+function run_cow {  
+    local ENV_DIR
+    local score
+    local state
+    local EYE
+    local TONGUE
+
+    ENV_DIR="$1"
+
+    fortune >/tmp/cow
+        
+    # 1. print message
+    # 2. remove newlines
+    # 3. leave out only ascii characters
+    # 4. remove the author (after -- <auhtor>)
+    # 5. call nltk to do the sentiment analysis
+    # 6. leave out the number after compound: 
+    # 7. normalize to range of -100..100
+
+    score=$(cat /tmp/cow | tr '\n' ' ' | tr -dc '[:print:]' | sed -e 's/\(.*\)--\(.*\)$/\1/g' | $ENV_DIR/n.py  2>/dev/null |  sed -e 's/compound: \([0-9\.\-]*\).*/\1/' |  awk '{printf "%.0f\n", $1 * 100 }')
+        
+    if [ $score -ge 60 ]; then
+           state="very good"
+           EYE="O"
+           TONGUE=":)" 
+    elif [ $score -ge 30 ]; then 
+           state="good"
+           EYE="o"
+           TONGUE=":)" 
+    elif [ $score -le -60 ]; then
+           state="very bad"
+           EYE="~"
+           TONGUE=":(" 
+    elif [ $score -le -30 ]; then
+           state="bad"
+           EYE="!"
+           TONGUE=":(" 
+    else
+           state="neutral"
+           EYE="."
+           TONGUE="--" 
+    fi
+
+    #not all cow shapes support eye and tongue character. strange
+    #random_shape="-f "$(cowsay -l | sed '1d' | tr ' ' '\n' | shuf -n 1 | awk '{ print $1 }') 
+
+    cat /tmp/cow | cowsay -e $EYE -T $TONGUE $random_shape
+    echo "nltk score: $score '(from -100..100) state: '$state' eye: $EYE tongue: $TONGUE"
+}
+
+function install_cow {
+    local ENV_DIR
+    local EDIR
+
+    ENV_DIR="$1"
+
+    echo "installing opinionated cow - first time install"
+    pushd $HOME
+
+    EDIR=$(basename ${ENV_DIR})
+    python3 -m venv $EDIR
+
+    bash -c "source ${ENV_DIR}/bin/activate; cd ${ENV_DIR}; pip install nltk"
+
+    cat >${ENV_DIR}/n.py <<EOF
+#!${ENV_DIR}/bin/python
+
+import sys
+import nltk
+
+nltk.downloader.download('vader_lexicon')
+
+from nltk.sentiment import SentimentIntensityAnalyzer
+
+def check_it(sentence):
+    #print("input {}".format( sentence ))
+    sid = SentimentIntensityAnalyzer()
+    ss = sid.polarity_scores(sentence)
+    for k in sorted(ss):
+        print('{0}: {1}, '.format(k, ss[k]), end='')
+    print()
+
+for line in sys.stdin:
+    check_it(line)
+EOF
+
+   chmod +x ${ENV_DIR}/n.py 
+    
+   popd
+}
+
+function opinionated_cow {
+ 
+    local ENV_DIR
+
+    ENV_DIR="$HOME/.opinionated-fortune-cow"
+
+    if [[ -d $ENV_DIR ]]; then
+        run_cow "$ENV_DIR"
+    else 
+        install_cow "$ENV_DIR"
+        run_cow "$ENV_DIR"
+    fi
+}
+
+opinionated_cow
